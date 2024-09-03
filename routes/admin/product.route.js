3109ebde1b6b7bd25355fea713177804c46454e5
John Doe <johndoe@email.com>
2024-09-03 21:05:29 +0700

    cloudinary

--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-//multer : đẩy ảnh hình với bộ lọc ở local
+//multer and cloudinary
 const multer = require('multer');
-const storageMulter = require('../../helpers/storageMulter');
-const upload = multer({  storage: storageMulter()});
+const upload = multer()
 
 const controller = require('../../controllers/admin/product.controller');
 const validate = require('../../validates/admin/product.validate');
+const uploadCloud = require('../../middlewares/admin/uploadCloudinary.middlewares');
 
 router.get('/', controller.index);
 
@@ -20,11 +20,15 @@ router.delete('/delete/:id', controller.deleteItem);
 router.get('/create', controller.create);
 
 // router.post('/create', controller.createPost); // khong su dung upload
-router.post('/create', upload.single('thumbnail'), validate.createPost, controller.createPost);
+router.post('/create', upload.single('thumbnail'),
+    uploadCloud.upload,
+    validate.createPost, 
+    controller.createPost
+);
 
 router.get('/edit/:id', controller.edit);
 
-router.patch('/edit/:id',  upload.single('thumbnail'), validate.createPost, controller.editPatch);
+router.patch('/edit/:id', upload.single('thumbnail'), validate.createPost, controller.editPatch);
 
 router.get("/detail/:id", controller.detail);
 
