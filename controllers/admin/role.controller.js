ad32cf306c196ef96a4ff498e28778338f3cdec3
John Doe <johndoe@email.com>
2024-09-09 14:51:49 +0700

    chỉnh sửa phân quyền

--- controllers/admin/role.controller.js
+++ controllers/admin/role.controller.js
@@ -30,3 +30,33 @@ module.exports.createPost = async (req, res) => {
 
     res.redirect(`${systemConfig.prefixAdmin}/roles`)
 }
+
+// [GET] /admin/roles/edit
+module.exports.edit = async (req, res) => {
+    try {
+        const id = req.params.id;
+        let find = {
+            deleted: false,
+            _id: id
+        }
+        const record = await Role.findOne(find);
+        res.render('admin/pages/roles/edit.pug', {
+            titlePage: "Chỉnh sửa nhóm quyền",
+            record: record
+        })
+    } catch (error) {
+        res.redirect(`${systemConfig.prefixAdmin}/roles`)
+    }
+}
+
+// [PATCH] /admin/roles/edit
+module.exports.editPatch = async (req, res) => {
+    try {
+        const id = req.params.id;
+        await Role.updateOne({ _id: id }, req.body);
+        req.flash("success", "Cấp nhật thanh cong nhóm quyên")
+    } catch (error) {
+        req.flash("error", "Cấp nhật that bai nhóm quyên")
+    }
+    res.redirect("back")
+}
