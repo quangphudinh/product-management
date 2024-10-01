0e443198f0e9f0b38db0e9ceabd9f9ffa426a587
John Doe <johndoe@email.com>
2024-10-01 14:04:40 +0700

    hiển thị tin nhắn đã lưu trong DB ra ngoài giao diện

--- controllers/client/chat.controller.js
+++ controllers/client/chat.controller.js
@@ -1,11 +1,34 @@
+const Chat = require('../../models/chat.model')
+const User = require('../../models/user.model')
 // [GET] /
 module.exports.index = async (req, res) => {
+    const userId = res.locals.user.id
     //socket
-    _io.on('connection', (socket) => {
-        console.log('a user connected', socket.id);
+    _io.once('connection', (socket) => {
+        socket.on('CLIENT_SEND_MESSAGE', async(content) => {
+            //luw vao db
+            const chat = new Chat({
+                user_id: userId,
+                content: content
+            });
+            await chat.save();
+        })
       });
+    //End socket
+    const chats = await Chat.find({
+        deleted: false
+    });
+    
+    for(const chat of chats){
+        const inforUser = await User.findOne({
+            _id: chat.user_id
+        }).select('fullName')
+        chat.inforUser = inforUser
+    }
+    //lay data tu DB
 
     res.render('client/pages/chat/index.pug', {
-        titlePage: 'Chat'
+        titlePage: 'Chat',
+        chats : chats
     })
 }
